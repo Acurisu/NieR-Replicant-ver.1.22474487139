06aa2fae7e2d11f71cc8b9d7d30c99d9663601bd
Acurisu <acurisu@gmail.com>
2026-03-15 22:59:39 +0100

    Updates

---
+++ _next/static/PeoSfCHz52VFMpyKZhlZc/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/NieR-Replicant-ver.1.22474487139//_next/:path+",
+        "destination": "/NieR-Replicant-ver.1.22474487139/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
