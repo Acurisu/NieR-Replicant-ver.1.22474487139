4b6c0683abb63a3e5c3b5bca4ab01d9047709eab
Acurisu <acurisu@gmail.com>
2026-05-13 14:45:26 +0200

    Bump editor dependencies

--- Editor/eslint.config.mjs
+++ Editor/eslint.config.mjs
@@ -1,10 +1,11 @@
+import { fixupConfigRules } from "@eslint/compat";
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
+  ...fixupConfigRules(nextVitals),
+  ...fixupConfigRules(nextTs),
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
